408e5dac509397cabc99f3d690bfc5071f90c6c7
App Academy Student <email@email.com>
2022-03-03 17:31:43 -0500

    finished shortner

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -5,6 +5,10 @@ include SecureRandom
   validates :long_url, :user_id, presence: true
   validates :short_url, uniqueness: true
 
+  belongs_to :submitter,
+    class_name: 'User',
+    foreign_key: :user_id,
+    primary_key: :id
 
   def self.random_code
     code = SecureRandom.urlsafe_base64
@@ -14,7 +18,12 @@ include SecureRandom
     code
   end
 
-  def shortener(user, long_url)
+  def self.shortener(user, long_url)
     code = ShortenedUrl.random_code
+    ShortenedUrl.create!(
+      long_url: long_url,
+      short_url: code,
+      user_id: user.id
+    )
   end
 end
