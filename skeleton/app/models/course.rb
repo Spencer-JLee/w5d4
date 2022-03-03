269f53790a3bca98058849cb8b9504b8a15a343e
App Academy Student <email@email.com>
2022-03-03 15:34:03 -0500

    finished associations project

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -7,4 +7,19 @@ class Course < ApplicationRecord
     foreign_key: :course_id,
     primary_key: :id
   )
+
+  belongs_to :prerequisite,
+    class_name: 'Course',
+    foreign_key: :prereq_id,
+    primary_key: :id,
+    optional: true
+
+  belongs_to(
+    :instructor,
+    class_name: 'User',
+    foreign_key: :instructor_id,
+    primary_key: :id
+  )
+
+  has_many :enrolled_students, through: :enrollments, source: :user
 end
--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -8,5 +8,13 @@ class User < ApplicationRecord
     primary_key: :id
   )
 
-  has_many :enrolled_courses, through: :enrollments, source: :course
+  has_many :courses_instructing,
+    class_name: 'Course',
+    foreign_key: :instructor_id,
+    primary_key: :id
+  
+
+  has_many :enrolled_courses, 
+    through: :enrollments, 
+    source: :course
 end
