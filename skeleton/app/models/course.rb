b9cda3d309da4fcea82607a8b917c3438c107c53
App Academy Student <email@email.com>
2022-03-03 14:23:14 -0500

    added associations for enrollment and users

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -1,2 +1,10 @@
 class Course < ApplicationRecord
+  validates :name, :instructor_id, presence: true
+  
+  has_many(
+    :enrollments,
+    class_name: 'Enrollment',
+    foreign_key: :course_id,
+    primary_key: :id
+  )
 end
--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+
+  has_many(
+    :enrollments,
+    class_name: 'Enrollment',
+    foreign_key: :student_id,
+    primary_key: :id
+  )
+
+  has_many :enrolled_courses, through: :enrollments, source: :course
 end
