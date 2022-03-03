aa3971101040b431283c6dda576e169734878679
App Academy Student <email@email.com>
2022-03-03 17:05:43 -0500

    working on securerandom

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,2 +1,12 @@
+require 'securerandom'
+
 class ShortenedUrl < ApplicationRecord
+include SecureRandom
+  validates :long_url, :user_id, presence: true
+  validates :short_url, uniqueness: true
+
+
+  def self.random_code
+    code = SecureRandom.urlsafe_base64
+  end
 end
--- URLShortener/db/migrate/20220303212142_create_shortened_urls.rb
+++ URLShortener/db/migrate/20220303212142_create_shortened_urls.rb
@@ -4,8 +4,12 @@ class CreateShortenedUrls < ActiveRecord::Migration[5.2]
       t.string :long_url, null: false
       t.string :short_url, unique: true
       t.integer :user_id, null: false
-
+      
       t.timestamps
     end
+    add_index(:shortened_urls, :long_url)
+    add_index :shortened_urls, :short_url
+    add_index :shortened_urls, :user_id
+    add_index :shortened_urls, [:long_url, :user_id], unique: true
   end
 end
